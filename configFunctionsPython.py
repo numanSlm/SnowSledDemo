a3bf20180bb3bb4d4596df0d35561942808f5207
Numan <numanslm99@gmail.com>
2024-07-25 01:29:15 -0400

    refactored with linter

--- configFunctionsPython.py
+++ configFunctionsPython.py
@@ -14,19 +14,19 @@ import pandas
 import logging
 
 
-
 def getConnProp():
     global configConn
     configConn = configparser.RawConfigParser()
-    configConn.read('./properties/configConnection.properties')
+    configConn.read("./properties/configConnection.properties")
     return
+
 
 def getPassProp():
     global configPass
     getExeProp()
     try:
-        password_location = configExecute.get('conditions', 'password_location')
-        print("Password location = ",password_location)
+        password_location = configExecute.get("conditions", "password_location")
+        print("Password location = ", password_location)
         configPass = configparser.RawConfigParser()
         configPass.read(password_location)
     except:
@@ -35,6 +35,7 @@ def getPassProp():
         exit()
     return
 
+
 # Add Email ID and Password below
 
 
@@ -42,40 +43,38 @@ def makeSSOConnection(connectionName):
     getConnProp()
     getPassProp()
     # Fetch  all values for sso snowflake connection
-    username = configConn.get(connectionName, 'username')
-    password = configPass.get(connectionName,'password')
-    warehouse = configConn.get(connectionName, 'warehouse')
-    database = configConn.get(connectionName, 'database')
-    schema = configConn.get(connectionName, 'schema')
-    role = configConn.get(connectionName, 'role')
-    account = configConn.get(connectionName, 'account')
-    authenticator = configConn.get(connectionName, 'authenticator')
-    client_id = configConn.get(connectionName, 'client_id')
-    client_secret = configConn.get(connectionName, 'client_secret')
-    grant_type = configConn.get(connectionName, 'grant_type')
-    scope = configConn.get(connectionName, 'scope')
-    url = configConn.get(connectionName, 'url')
+    username = configConn.get(connectionName, "username")
+    password = configPass.get(connectionName, "password")
+    warehouse = configConn.get(connectionName, "warehouse")
+    database = configConn.get(connectionName, "database")
+    schema = configConn.get(connectionName, "schema")
+    role = configConn.get(connectionName, "role")
+    account = configConn.get(connectionName, "account")
+    authenticator = configConn.get(connectionName, "authenticator")
+    client_id = configConn.get(connectionName, "client_id")
+    client_secret = configConn.get(connectionName, "client_secret")
+    grant_type = configConn.get(connectionName, "grant_type")
+    scope = configConn.get(connectionName, "scope")
+    url = configConn.get(connectionName, "url")
 
     # Parameters to pass to get access token
     data2 = {
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'username': username,
-        'password': password,
-        'grant_type': grant_type,
-        'scope': scope
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "username": username,
+        "password": password,
+        "grant_type": grant_type,
+        "scope": scope,
     }
 
     # Get access-token from AWS privatelink (DO NOT CHANGE)
-    headers = {
-        'Content-type': 'application/x-www-form-urlencoded;charset=UTF-8'
-    }
-    
+    headers = {"Content-type": "application/x-www-form-urlencoded;charset=UTF-8"}
+
     response = requests.post(url, headers=headers, data=data2)
 
     response_json = json.loads(response.text)
     try:
-        access_token = response_json['access_token']
+        access_token = response_json["access_token"]
         print("Access token generated")
     except:
         print("Access token not received")
@@ -90,9 +89,9 @@ def makeSSOConnection(connectionName):
         role=role,
         database=database,
         schema=schema,
-        token=access_token
+        token=access_token,
     )
-    print("Logging in for User: ",username)
+    print("Logging in for User: ", username)
     return cursor_target
 
 
@@ -100,14 +99,14 @@ def makeSnowflakeConnection(connectionName):
     getConnProp()
     getPassProp()
 
-    warehouse = configConn.get(connectionName, 'warehouse')
-    username = configConn.get(connectionName, 'username')
-    password = configConn.get(connectionName,'password')
-    database = configConn.get(connectionName, 'database')
-    account = configConn.get(connectionName, 'account')
-    schema = configConn.get(connectionName, 'schema')
-    role = configConn.get(connectionName, 'role')
-    
+    warehouse = configConn.get(connectionName, "warehouse")
+    username = configConn.get(connectionName, "username")
+    password = configConn.get(connectionName, "password")
+    database = configConn.get(connectionName, "database")
+    account = configConn.get(connectionName, "account")
+    schema = configConn.get(connectionName, "schema")
+    role = configConn.get(connectionName, "role")
+
     cursor_target = snowflake.connector.connect(
         user=username,
         password=password,
@@ -115,114 +114,143 @@ def makeSnowflakeConnection(connectionName):
         warehouse=warehouse,
         database=database,
         schema=schema,
-        role=role
+        role=role,
     )
-    print("Logging in for User: ",username)
+    print("Logging in for User: ", username)
     return cursor_target
 
 
 def getExeProp():
     global configExecute
     configExecute = configparser.RawConfigParser()
-    configExecute.read('./properties/configExecution.properties')
+    configExecute.read("./properties/configExecution.properties")
     return
-
-
 
 
 def getExecuteConfig():
     getExeProp()
-    print("-----------------------------------------------------------------------------")
+    print(
+        "-----------------------------------------------------------------------------"
+    )
     try:
-        sso_login = configExecute.get('conditions', 'sso_login')
-        print("SSO_LOGIN = ",sso_login)
+        sso_login = configExecute.get("conditions", "sso_login")
+        print("SSO_LOGIN = ", sso_login)
     except:
-        sso_login = 'FALSE'
-        print("Could not find SSO_LOGIN parameter in configExecute.properties, Using Non SSO Connection")
+        sso_login = "FALSE"
+        print(
+            "Could not find SSO_LOGIN parameter in configExecute.properties, Using Non SSO Connection"
+        )
     try:
-        continue_on_failure = configExecute.get('conditions', 'continue_on_failure')
-        print("Continue on Failure = ",continue_on_failure)
+        continue_on_failure = configExecute.get("conditions", "continue_on_failure")
+        print("Continue on Failure = ", continue_on_failure)
     except:
-        continue_on_failure = 'FALSE'
-        print("Could not find continue_on_failure parameter, Using Default (FALSE) Value. ")
+        continue_on_failure = "FALSE"
+        print(
+            "Could not find continue_on_failure parameter, Using Default (FALSE) Value. "
+        )
     try:
-        variable_substitution = configExecute.get('conditions', 'variable_substitution')
-        print("Variable Substitution = ",variable_substitution)
+        variable_substitution = configExecute.get("conditions", "variable_substitution")
+        print("Variable Substitution = ", variable_substitution)
     except:
-        variable_substitution = 'FALSE'
-        print("Could not find variable_substitution parameter, Using Default (FALSE) Value. ")
+        variable_substitution = "FALSE"
+        print(
+            "Could not find variable_substitution parameter, Using Default (FALSE) Value. "
+        )
     try:
-        show_errors_on_console = configExecute.get('conditions', 'show_errors_on_console')
-        print("Errors on Console = ",show_errors_on_console)
+        show_errors_on_console = configExecute.get(
+            "conditions", "show_errors_on_console"
+        )
+        print("Errors on Console = ", show_errors_on_console)
     except:
-        show_errors_on_console = 'FALSE'
-        print("Could not find show_errors_on_console parameter, Using Default (FALSE) Value. ")
+        show_errors_on_console = "FALSE"
+        print(
+            "Could not find show_errors_on_console parameter, Using Default (FALSE) Value. "
+        )
     try:
-        append_master_csv_log = configExecute.get('conditions', 'append_master_csv_log')
-        print("Append Output to MASTER LOG = ",append_master_csv_log)
+        append_master_csv_log = configExecute.get("conditions", "append_master_csv_log")
+        print("Append Output to MASTER LOG = ", append_master_csv_log)
     except:
-        append_master_csv_log = 'FALSE'
-        print("Could not find append_master_csv_log parameter, Using Default (FALSE) Value. ")
-#    try:
-#        default_input_file_source = configExecute.get('conditions', 'default_input_file_source')
-#        print("Default source = ",default_input_file_source)
-#    except:
-#        default_input_file_source = "TRUE"
-#        print("Could not find default_input_file_source parameter, Using Default (TRUE) Value. ")
+        append_master_csv_log = "FALSE"
+        print(
+            "Could not find append_master_csv_log parameter, Using Default (FALSE) Value. "
+        )
+    #    try:
+    #        default_input_file_source = configExecute.get('conditions', 'default_input_file_source')
+    #        print("Default source = ",default_input_file_source)
+    #    except:
+    #        default_input_file_source = "TRUE"
+    #        print("Could not find default_input_file_source parameter, Using Default (TRUE) Value. ")
     try:
-        password_location = configExecute.get('conditions', 'password_location')
-        print("Password location = ",password_location)
+        password_location = configExecute.get("conditions", "password_location")
+        print("Password location = ", password_location)
     except:
         password_location = "Location not found"
         print("Could not find Password in password location parameter.")
         exit()
     try:
-        split_on_parameter = configExecute.get('conditions', 'split_on_parameter')
-        print("split_on_parameter = ",split_on_parameter)
+        split_on_parameter = configExecute.get("conditions", "split_on_parameter")
+        print("split_on_parameter = ", split_on_parameter)
     except:
         split_on_parameter = "TRUE"
-        print("Could not find split_on_parameter parameter, Using Default (TRUE) Value.")
+        print(
+            "Could not find split_on_parameter parameter, Using Default (TRUE) Value."
+        )
     try:
-        full_path_provided = configExecute.get('conditions', 'full_path_provided')
-        print("Password location = ",full_path_provided)
+        full_path_provided = configExecute.get("conditions", "full_path_provided")
+        print("Password location = ", full_path_provided)
     except:
         full_path_provided = "FALSE"
-        print("Could not find full_path_provided parameter, Using Default (TRUE) Value.")
- 
-    try:
-        default_connection = 'snowflake_direct'
-        default_connection = configExecute.get('conditions', 'default_connection')
-        print("Default Connection Name = ",default_connection)
-    except:
-        default_connection = 'snowflake_direct'
-        #print("Could not find default_connection parameter, Using Default (default_connection) Value.")
-#
-    try:
-        default_file_list_name = configExecute.get('conditions', 'default_file_list_name')
-        print("Default File Name = ",default_file_list_name)
-    except:
-        default_file_list_name = "executionFileNames.txt"
-        #print("Could not find default_file_list_name parameter, Using Default (executionFileNames.txt) Value.")
+        print(
+            "Could not find full_path_provided parameter, Using Default (TRUE) Value."
+        )
 
     try:
-        splitting_parameter = configExecute.get('conditions', 'splitting_parameter')
-        print("Splitting parameter set to = ",splitting_parameter)
+        default_connection = "snowflake_direct"
+        default_connection = configExecute.get("conditions", "default_connection")
+        print("Default Connection Name = ", default_connection)
+    except:
+        default_connection = "snowflake_direct"
+        # print("Could not find default_connection parameter, Using Default (default_connection) Value.")
+    #
+    try:
+        default_file_list_name = configExecute.get(
+            "conditions", "default_file_list_name"
+        )
+        print("Default File Name = ", default_file_list_name)
+    except:
+        default_file_list_name = "executionFileNames.txt"
+        # print("Could not find default_file_list_name parameter, Using Default (executionFileNames.txt) Value.")
+
+    try:
+        splitting_parameter = configExecute.get("conditions", "splitting_parameter")
+        print("Splitting parameter set to = ", splitting_parameter)
     except:
         splitting_parameter = ";"
         print("Could not find splitting_parameter parameter, Using Default (;) Value.")
 
-            
-    print("-----------------------------------------------------------------------------")
-    return sso_login,continue_on_failure,variable_substitution,show_errors_on_console,append_master_csv_log,full_path_provided,split_on_parameter,default_connection,default_file_list_name,splitting_parameter
+    print(
+        "-----------------------------------------------------------------------------"
+    )
+    return (
+        sso_login,
+        continue_on_failure,
+        variable_substitution,
+        show_errors_on_console,
+        append_master_csv_log,
+        full_path_provided,
+        split_on_parameter,
+        default_connection,
+        default_file_list_name,
+        splitting_parameter,
+    )
 
 
 def substitueVariable(sql):
-    with open('./properties/Mapping.csv', mode='r', encoding='utf-8-sig') as infile:
+    with open("./properties/Mapping.csv", mode="r", encoding="utf-8-sig") as infile:
         reader = csv.reader(infile)
         # Store values in mapping dictionary
         dictSimpleFunctions = {rows[0]: rows[1] for rows in reader}
     for input_value, convert_value in dictSimpleFunctions.items():
-        replace_value = "'&"+input_value.strip()+"'"
+        replace_value = "'&" + input_value.strip() + "'"
         sql = sql.replace(replace_value, convert_value.strip())
     return sql
-
--- snowflakeExecutor.py
+++ snowflakeExecutor.py
@@ -10,31 +10,44 @@ from os import path
 import time
 
 # Get execution configurations parameters
-sso_login, continue_on_failure, variable_substitution, show_errors_on_console, append_master_csv_log, full_path_provided, split_on_parameter, default_connection, default_file_list_name, splitting_parameter = getExecuteConfig()
+(
+    sso_login,
+    continue_on_failure,
+    variable_substitution,
+    show_errors_on_console,
+    append_master_csv_log,
+    full_path_provided,
+    split_on_parameter,
+    default_connection,
+    default_file_list_name,
+    splitting_parameter,
+) = getExecuteConfig()
 inputFileList = ""
 
 
 fFlagUnset = False
 for i in range(0, len(sys.argv)):
-    if(sys.argv[i] == "-f"):
-        inputFileList = sys.argv[i+1]
+    if sys.argv[i] == "-f":
+        inputFileList = sys.argv[i + 1]
         fFlagUnset = True
         break
-if(fFlagUnset == False):
+if fFlagUnset == False:
     inputFileList = default_file_list_name
 
 cFlagUnset = False
 for i in range(0, len(sys.argv)):
-    if(sys.argv[i] == "-c"):
-        connectionName = sys.argv[i+1]
+    if sys.argv[i] == "-c":
+        connectionName = sys.argv[i + 1]
         cFlagUnset = True
         break
 
-if(cFlagUnset == False):
+if cFlagUnset == False:
     connectionName = default_connection
 
-if(len(inputFileList) > 0):
-    print("-----------------------------------------------------------------------------")
+if len(inputFileList) > 0:
+    print(
+        "-----------------------------------------------------------------------------"
+    )
     print("Reading files from", inputFileList)
 
 
@@ -42,7 +55,7 @@ if(len(inputFileList) > 0):
 # sso_login, continue_on_failure, variable_substitution, show_errors_on_console, append_master_csv_log = checkConfigCondition = getExecuteConfig()
 
 # Check connection type
-if(sso_login.strip().upper() == 'TRUE'):
+if sso_login.strip().upper() == "TRUE":
     # Create a connection
     cursor_target = makeSSOConnection(connectionName)
     print("Snowflake SSO Connection Established :", connectionName)
@@ -55,74 +68,86 @@ else:
 cs = cursor_target.cursor()
 
 # Initiate Logger
-for logger_name in ['snowflake.connector', 'botocore', 'boto3']:
+for logger_name in ["snowflake.connector", "botocore", "boto3"]:
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.DEBUG)
     # Change Logger File if needed
-    ch = logging.FileHandler('./Output/LoggerDEBUG.log')
+    ch = logging.FileHandler("./Output/LoggerDEBUG.log")
     ch.setLevel(logging.DEBUG)
-    ch.setFormatter(SecretDetector(
-        '%(asctime)s - %(threadName)s %(filename)s:%(lineno)d - %(funcName)s() - %(levelname)s - %(message)s'))
+    ch.setFormatter(
+        SecretDetector(
+            "%(asctime)s - %(threadName)s %(filename)s:%(lineno)d - %(funcName)s() - %(levelname)s - %(message)s"
+        )
+    )
     logger.addHandler(ch)
 
 # Check Output Logging File Name
-mode = 'w'
-if(append_master_csv_log.strip().upper() == 'TRUE'):
+mode = "w"
+if append_master_csv_log.strip().upper() == "TRUE":
     OutputLog = "MASTER_LOG.csv"
-    mode = 'a'
+    mode = "a"
 else:
-    value = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
-    OutputLog = str(value)+"_log.csv"
-    mode = 'w'
+    value = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    OutputLog = str(value) + "_log.csv"
+    mode = "w"
 
 # Read all execution file names
-with open('./'+inputFileList, mode='r', encoding='utf-8-sig') as infile:
+with open("./" + inputFileList, mode="r", encoding="utf-8-sig") as infile:
     reader = infile.read()
 infile.close()
 
 # List of all Files
-executionFileNames = reader.split('\n')
+executionFileNames = reader.split("\n")
 executedTotal = 0
 executionFailure = 0
 
 dontAddHeader = False
 try:
-    with open('./output/'+OutputLog, mode='r', encoding='utf-8') as fd2:
+    with open("./output/" + OutputLog, mode="r", encoding="utf-8") as fd2:
         readFile = fd2.read()
-        if(len(readFile.strip()) > 0):
+        if len(readFile.strip()) > 0:
             dontAddHeader = True
 except:
     dontAddHeader = False
 
 
 # Write into Output file
-with open('./output/'+OutputLog, mode=mode, encoding='utf-8', newline='') as fd:
-    writer = csv.writer(fd, delimiter=',')
-    if(dontAddHeader == False):
-        writer.writerow(["File,Name", "ObjectName", "Execution",
-                         "Timestamp", "ErrorLog", "Failed_SFQID", "ExecutionTime(mins)"])
+with open("./output/" + OutputLog, mode=mode, encoding="utf-8", newline="") as fd:
+    writer = csv.writer(fd, delimiter=",")
+    if dontAddHeader == False:
+        writer.writerow(
+            [
+                "File,Name",
+                "ObjectName",
+                "Execution",
+                "Timestamp",
+                "ErrorLog",
+                "Failed_SFQID",
+                "ExecutionTime(mins)",
+            ]
+        )
 
     # Iteragte through each file
     for fileName in executionFileNames:
         skipFile = False
         errorsInFile = 0
-        if(fileName.startswith('#')):
+        if fileName.startswith("#"):
             print("Skipped ", fileName)
             skipFile = True
         else:
             # add things here
-            if(full_path_provided.strip().upper() == 'FALSE'):
-                input_path = "./input/"+fileName
+            if full_path_provided.strip().upper() == "FALSE":
+                input_path = "./input/" + fileName
             else:
                 input_path = fileName
 
-            if(path.exists(input_path)):
-                with open(input_path, mode='r', encoding='utf-8') as readfile:
+            if path.exists(input_path):
+                with open(input_path, mode="r", encoding="utf-8") as readfile:
                     # Read Data inside file
                     fileContent = readfile.read()
 
                 # Split on each semicolon
-                if(split_on_parameter.upper().strip() == "TRUE"):
+                if split_on_parameter.upper().strip() == "TRUE":
                     sqlQueries = fileContent.split(splitting_parameter)
                 else:
                     sqlQueries = []
@@ -138,19 +163,19 @@ with open('./output/'+OutputLog, mode=mode, encoding='utf-8', newline='') as fd:
                     sql = sql.strip()
                     codeTerminated = False
                     # tempTime used in Logs for estimated query execution time
-                    tempTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                    tempTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
                     # Remove comments
                     sql = sqlparse.format(sql, strip_comments=True).strip()
 
                     # Check for empty string
-                    if(len(sql) > 0):
+                    if len(sql) > 0:
 
                         # Increment Total SQL Executed Queries Count
-                        executedTotal = executedTotal+1
+                        executedTotal = executedTotal + 1
 
                         # Check if variables need to be substituted
-                        if(variable_substitution.strip().upper() == 'TRUE'):
+                        if variable_substitution.strip().upper() == "TRUE":
                             sql = substitueVariable(sql)
 
                         # Execute the sql query
@@ -159,27 +184,44 @@ with open('./output/'+OutputLog, mode=mode, encoding='utf-8', newline='') as fd:
                             startTime = time.time()
                             output = cs.execute(sql)
                             endTime = time.time()
-                            totalExecutionTime = (endTime-startTime)/60
+                            totalExecutionTime = (endTime - startTime) / 60
                             print("Elapsed time(in min):", totalExecutionTime)
                             # Write into file
                             writer.writerow(
-                                [fileName, sql, "SUCCESS", str(tempTime), "", "", totalExecutionTime])
+                                [
+                                    fileName,
+                                    sql,
+                                    "SUCCESS",
+                                    str(tempTime),
+                                    "",
+                                    "",
+                                    totalExecutionTime,
+                                ]
+                            )
 
                         except snowflake.connector.errors.ProgrammingError as e:
                             # Capture the error (Remove extra tabs and newline from error msg)
-                            error = e.msg.replace('\n', ' ')
-                            error = error.replace('\t', ' ')
+                            error = e.msg.replace("\n", " ")
+                            error = error.replace("\t", " ")
 
                             # Increment failure count
-                            executionFailure = executionFailure+1
-                            errorsInFile = errorsInFile+1
+                            executionFailure = executionFailure + 1
+                            errorsInFile = errorsInFile + 1
                             # Write into file
-                            writer.writerow([fileName, sql, "FAIL", str(
-                                tempTime), str(error), str(e.sfqid)])
+                            writer.writerow(
+                                [
+                                    fileName,
+                                    sql,
+                                    "FAIL",
+                                    str(tempTime),
+                                    str(error),
+                                    str(e.sfqid),
+                                ]
+                            )
                             error_flag = True
 
                             # Display errors if show_errors_on_console is true
-                            if(show_errors_on_console.strip().upper() == "TRUE"):
+                            if show_errors_on_console.strip().upper() == "TRUE":
                                 print("----------------------------------")
                                 print("ERROR IN FILE: ", fileName)
                                 print("----------------------------------")
@@ -191,33 +233,45 @@ with open('./output/'+OutputLog, mode=mode, encoding='utf-8', newline='') as fd:
                                 print("----------------------------------")
 
                     # Check condition to break execution based on config property
-                    if(continue_on_failure.strip().upper() == "FALSE" and error_flag == True):
+                    if (
+                        continue_on_failure.strip().upper() == "FALSE"
+                        and error_flag == True
+                    ):
 
                         # Unusual termination of code
                         codeTerminated = True
                         break
-                if(continue_on_failure.strip().upper() == "FALSE" and error_flag == True):
+                if (
+                    continue_on_failure.strip().upper() == "FALSE"
+                    and error_flag == True
+                ):
                     # Unusual termination of code
                     codeTerminated = True
                     break
 
-                if(errorsInFile == 0 and skipFile == False):
+                if errorsInFile == 0 and skipFile == False:
                     print(fileName, "Executed Succesfully.")
-                if(codeTerminated == False and errorsInFile > 0 and skipFile == False):
-                    print("For File:", fileName, "A total of", errorsInFile,
-                          "queries did not run successfully. ")
+                if codeTerminated == False and errorsInFile > 0 and skipFile == False:
+                    print(
+                        "For File:",
+                        fileName,
+                        "A total of",
+                        errorsInFile,
+                        "queries did not run successfully. ",
+                    )
                 print("-------------------------------------------")
             else:
                 # Can add counters here if required
                 print(input_path, " PATH not found")
-    if(codeTerminated == True):
+    if codeTerminated == True:
         print(fileName, "Execution Terminated. (Please check logs)")
-    print("-----------------------------------------------------------------------------")
+    print(
+        "-----------------------------------------------------------------------------"
+    )
 
 print("\tFind Logs for this execution at: ", OutputLog.strip())
 print("-----------------------------------------------------------------------------")
-print("\t\t\tExecution Stats: ", executedTotal -
-      executionFailure, "/", executedTotal)
+print("\t\t\tExecution Stats: ", executedTotal - executionFailure, "/", executedTotal)
 print("\t\t\tFailed Queries: ", executionFailure)
 print("-----------------------------------------------------------------------------")
 print("-------- Thank you for using Snowflake Python Executor --------")
