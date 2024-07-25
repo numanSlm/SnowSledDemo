b5b535c421446e9ec9531af705fed0fea27e51b8
Numan <numanslm99@gmail.com>
2024-07-25 01:47:57 -0400

    Update configFunctionsPython.py

--- configFunctionsPython.py
+++ configFunctionsPython.py
@@ -1,64 +1,53 @@
-"""
-
-"""
-
 import csv
-import os
 import configparser
 import snowflake.connector
-import sqlparse
-from datetime import datetime
 import requests
-import json
-import pandas
 import logging
 
-
-def getConnProp():
-    global configConn
-    configConn = configparser.RawConfigParser()
-    configConn.read("./properties/configConnection.properties")
-    return
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
 
 
-def getPassProp():
-    global configPass
-    getExeProp()
+def get_conn_prop():
+    config_conn = configparser.RawConfigParser()
+    config_conn.read("./properties/configConnection.properties")
+    return config_conn
+
+
+def get_pass_prop(config_execute):
     try:
-        password_location = configExecute.get("conditions", "password_location")
-        print("Password location = ", password_location)
-        configPass = configparser.RawConfigParser()
-        configPass.read(password_location)
-    except:
-        password_location = "Not found"
-        print("Could not find password in location mentioned.")
+        password_location = config_execute.get("conditions", "password_location")
+        logging.info("Password location = %s", password_location)
+        config_pass = configparser.RawConfigParser()
+        config_pass.read(password_location)
+        return config_pass
+    except configparser.NoOptionError:
+        logging.error("Could not find password in location mentioned.")
         exit()
-    return
 
 
-# Add Email ID and Password below
+def make_sso_connection(connection_name):
+    config_conn = get_conn_prop()
+    config_execute = get_exe_prop()
+    config_pass = get_pass_prop(config_execute)
 
-
-def makeSSOConnection(connectionName):
-    getConnProp()
-    getPassProp()
-    # Fetch  all values for sso snowflake connection
-    username = configConn.get(connectionName, "username")
-    password = configPass.get(connectionName, "password")
-    warehouse = configConn.get(connectionName, "warehouse")
-    database = configConn.get(connectionName, "database")
-    schema = configConn.get(connectionName, "schema")
-    role = configConn.get(connectionName, "role")
-    account = configConn.get(connectionName, "account")
-    authenticator = configConn.get(connectionName, "authenticator")
-    client_id = configConn.get(connectionName, "client_id")
-    client_secret = configConn.get(connectionName, "client_secret")
-    grant_type = configConn.get(connectionName, "grant_type")
-    scope = configConn.get(connectionName, "scope")
-    url = configConn.get(connectionName, "url")
+    # Fetch all values for SSO Snowflake connection
+    username = config_conn.get(connection_name, "username")
+    password = config_pass.get(connection_name, "password")
+    warehouse = config_conn.get(connection_name, "warehouse")
+    database = config_conn.get(connection_name, "database")
+    schema = config_conn.get(connection_name, "schema")
+    role = config_conn.get(connection_name, "role")
+    account = config_conn.get(connection_name, "account")
+    authenticator = config_conn.get(connection_name, "authenticator")
+    client_id = config_conn.get(connection_name, "client_id")
+    client_secret = config_conn.get(connection_name, "client_secret")
+    grant_type = config_conn.get(connection_name, "grant_type")
+    scope = config_conn.get(connection_name, "scope")
+    url = config_conn.get(connection_name, "url")
 
     # Parameters to pass to get access token
-    data2 = {
+    data = {
         "client_id": client_id,
         "client_secret": client_secret,
         "username": username,
@@ -70,17 +59,17 @@ def makeSSOConnection(connectionName):
     # Get access-token from AWS privatelink (DO NOT CHANGE)
     headers = {"Content-type": "application/x-www-form-urlencoded;charset=UTF-8"}
 
-    response = requests.post(url, headers=headers, data=data2)
+    response = requests.post(url, headers=headers, data=data)
+    response_json = response.json()
 
-    response_json = json.loads(response.text)
     try:
         access_token = response_json["access_token"]
-        print("Access token generated")
-    except:
-        print("Access token not received")
+        logging.info("Access token generated")
+    except KeyError:
+        logging.error("Access token not received")
         exit()
 
-    # Change user,warehouse,role,database,schema as required below
+    # Change user, warehouse, role, database, schema as required below
     cursor_target = snowflake.connector.connect(
         user=username,
         account=account,
@@ -91,21 +80,22 @@ def makeSSOConnection(connectionName):
         schema=schema,
         token=access_token,
     )
-    print("Logging in for User: ", username)
+    logging.info("Logging in for User: %s", username)
     return cursor_target
 
 
-def makeSnowflakeConnection(connectionName):
-    getConnProp()
-    getPassProp()
+def make_snowflake_connection(connection_name):
+    config_conn = get_conn_prop()
+    config_execute = get_exe_prop()
+    config_pass = get_pass_prop(config_execute)
 
-    warehouse = configConn.get(connectionName, "warehouse")
-    username = configConn.get(connectionName, "username")
-    password = configConn.get(connectionName, "password")
-    database = configConn.get(connectionName, "database")
-    account = configConn.get(connectionName, "account")
-    schema = configConn.get(connectionName, "schema")
-    role = configConn.get(connectionName, "role")
+    warehouse = config_conn.get(connection_name, "warehouse")
+    username = config_conn.get(connection_name, "username")
+    password = config_pass.get(connection_name, "password")
+    database = config_conn.get(connection_name, "database")
+    account = config_conn.get(connection_name, "account")
+    schema = config_conn.get(connection_name, "schema")
+    role = config_conn.get(connection_name, "role")
 
     cursor_target = snowflake.connector.connect(
         user=username,
@@ -116,121 +106,97 @@ def makeSnowflakeConnection(connectionName):
         schema=schema,
         role=role,
     )
-    print("Logging in for User: ", username)
+    logging.info("Logging in for User: %s", username)
     return cursor_target
 
 
-def getExeProp():
-    global configExecute
-    configExecute = configparser.RawConfigParser()
-    configExecute.read("./properties/configExecution.properties")
-    return
+def get_exe_prop():
+    config_execute = configparser.RawConfigParser()
+    config_execute.read("./properties/configExecution.properties")
+    return config_execute
 
 
-def getExecuteConfig():
-    getExeProp()
-    print(
-        "-----------------------------------------------------------------------------"
-    )
+def get_execute_config():
+    config_execute = get_exe_prop()
+    logging.info("-----------------------------------------------------------------------------")
+
     try:
-        sso_login = configExecute.get("conditions", "sso_login")
-        print("SSO_LOGIN = ", sso_login)
-    except:
+        sso_login = config_execute.get("conditions", "sso_login")
+        logging.info("SSO_LOGIN = %s", sso_login)
+    except configparser.NoOptionError:
         sso_login = "FALSE"
-        print(
-            "Could not find SSO_LOGIN parameter in configExecute.properties, Using Non SSO Connection"
-        )
+        logging.warning("Could not find SSO_LOGIN parameter, Using Non SSO Connection")
+
     try:
-        continue_on_failure = configExecute.get("conditions", "continue_on_failure")
-        print("Continue on Failure = ", continue_on_failure)
-    except:
+        continue_on_failure = config_execute.get("conditions", "continue_on_failure")
+        logging.info("Continue on Failure = %s", continue_on_failure)
+    except configparser.NoOptionError:
         continue_on_failure = "FALSE"
-        print(
-            "Could not find continue_on_failure parameter, Using Default (FALSE) Value. "
-        )
+        logging.warning("Could not find continue_on_failure parameter, Using Default (FALSE) Value.")
+
     try:
-        variable_substitution = configExecute.get("conditions", "variable_substitution")
-        print("Variable Substitution = ", variable_substitution)
-    except:
+        variable_substitution = config_execute.get("conditions", "variable_substitution")
+        logging.info("Variable Substitution = %s", variable_substitution)
+    except configparser.NoOptionError:
         variable_substitution = "FALSE"
-        print(
-            "Could not find variable_substitution parameter, Using Default (FALSE) Value. "
-        )
+        logging.warning("Could not find variable_substitution parameter, Using Default (FALSE) Value.")
+
     try:
-        show_errors_on_console = configExecute.get(
-            "conditions", "show_errors_on_console"
-        )
-        print("Errors on Console = ", show_errors_on_console)
-    except:
+        show_errors_on_console = config_execute.get("conditions", "show_errors_on_console")
+        logging.info("Errors on Console = %s", show_errors_on_console)
+    except configparser.NoOptionError:
         show_errors_on_console = "FALSE"
-        print(
-            "Could not find show_errors_on_console parameter, Using Default (FALSE) Value. "
-        )
+        logging.warning("Could not find show_errors_on_console parameter, Using Default (FALSE) Value.")
+
     try:
-        append_master_csv_log = configExecute.get("conditions", "append_master_csv_log")
-        print("Append Output to MASTER LOG = ", append_master_csv_log)
-    except:
+        append_master_csv_log = config_execute.get("conditions", "append_master_csv_log")
+        logging.info("Append Output to MASTER LOG = %s", append_master_csv_log)
+    except configparser.NoOptionError:
         append_master_csv_log = "FALSE"
-        print(
-            "Could not find append_master_csv_log parameter, Using Default (FALSE) Value. "
-        )
-    #    try:
-    #        default_input_file_source = configExecute.get('conditions', 'default_input_file_source')
-    #        print("Default source = ",default_input_file_source)
-    #    except:
-    #        default_input_file_source = "TRUE"
-    #        print("Could not find default_input_file_source parameter, Using Default (TRUE) Value. ")
+        logging.warning("Could not find append_master_csv_log parameter, Using Default (FALSE) Value.")
+
     try:
-        password_location = configExecute.get("conditions", "password_location")
-        print("Password location = ", password_location)
-    except:
+        password_location = config_execute.get("conditions", "password_location")
+        logging.info("Password location = %s", password_location)
+    except configparser.NoOptionError:
         password_location = "Location not found"
-        print("Could not find Password in password location parameter.")
+        logging.error("Could not find Password in password location parameter.")
         exit()
+
     try:
-        split_on_parameter = configExecute.get("conditions", "split_on_parameter")
-        print("split_on_parameter = ", split_on_parameter)
-    except:
+        split_on_parameter = config_execute.get("conditions", "split_on_parameter")
+        logging.info("split_on_parameter = %s", split_on_parameter)
+    except configparser.NoOptionError:
         split_on_parameter = "TRUE"
-        print(
-            "Could not find split_on_parameter parameter, Using Default (TRUE) Value."
-        )
+        logging.warning("Could not find split_on_parameter parameter, Using Default (TRUE) Value.")
+
     try:
-        full_path_provided = configExecute.get("conditions", "full_path_provided")
-        print("Password location = ", full_path_provided)
-    except:
+        full_path_provided = config_execute.get("conditions", "full_path_provided")
+        logging.info("Password location = %s", full_path_provided)
+    except configparser.NoOptionError:
         full_path_provided = "FALSE"
-        print(
-            "Could not find full_path_provided parameter, Using Default (TRUE) Value."
-        )
+        logging.warning("Could not find full_path_provided parameter, Using Default (TRUE) Value.")
 
     try:
+        default_connection = config_execute.get("conditions", "default_connection")
+        logging.info("Default Connection Name = %s", default_connection)
+    except configparser.NoOptionError:
         default_connection = "snowflake_direct"
-        default_connection = configExecute.get("conditions", "default_connection")
-        print("Default Connection Name = ", default_connection)
-    except:
-        default_connection = "snowflake_direct"
-        # print("Could not find default_connection parameter, Using Default (default_connection) Value.")
-    #
+
     try:
-        default_file_list_name = configExecute.get(
-            "conditions", "default_file_list_name"
-        )
-        print("Default File Name = ", default_file_list_name)
-    except:
+        default_file_list_name = config_execute.get("conditions", "default_file_list_name")
+        logging.info("Default File Name = %s", default_file_list_name)
+    except configparser.NoOptionError:
         default_file_list_name = "executionFileNames.txt"
-        # print("Could not find default_file_list_name parameter, Using Default (executionFileNames.txt) Value.")
 
     try:
-        splitting_parameter = configExecute.get("conditions", "splitting_parameter")
-        print("Splitting parameter set to = ", splitting_parameter)
-    except:
+        splitting_parameter = config_execute.get("conditions", "splitting_parameter")
+        logging.info("Splitting parameter set to = %s", splitting_parameter)
+    except configparser.NoOptionError:
         splitting_parameter = ";"
-        print("Could not find splitting_parameter parameter, Using Default (;) Value.")
+        logging.warning("Could not find splitting_parameter parameter, Using Default (;) Value.")
 
-    print(
-        "-----------------------------------------------------------------------------"
-    )
+    logging.info("-----------------------------------------------------------------------------")
     return (
         sso_login,
         continue_on_failure,
@@ -245,12 +211,12 @@ def getExecuteConfig():
     )
 
 
-def substitueVariable(sql):
+def substitute_variable(sql):
     with open("./properties/Mapping.csv", mode="r", encoding="utf-8-sig") as infile:
         reader = csv.reader(infile)
         # Store values in mapping dictionary
-        dictSimpleFunctions = {rows[0]: rows[1] for rows in reader}
-    for input_value, convert_value in dictSimpleFunctions.items():
+        dict_simple_functions = {rows[0]: rows[1] for rows in reader}
+    for input_value, convert_value in dict_simple_functions.items():
         replace_value = "'&" + input_value.strip() + "'"
         sql = sql.replace(replace_value, convert_value.strip())
     return sql
